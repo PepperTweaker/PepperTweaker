25af1c63d016f27a5ce9eb874cab4adebfa801ac
bearbyt3z <bearbyt3z@gmail.com>
2024-03-22 03:24:46 +0100

    Added a grey border around the user profile header box

--- PepperTweaker.user.js
+++ PepperTweaker.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         PepperTweaker
 // @namespace    bearbyt3z
-// @version      0.9.92
+// @version      0.9.93
 // @description  Pepper na resorach...
 // @author       bearbyt3z
 // @match        https://www.pepper.pl/*
@@ -465,6 +465,9 @@
         /* END: Faders */
         .btn--border, .bg--off, .boxSec--fromW3:not(.thread-infos), .boxSec, .voucher-codeCopyButton, .search input, .img, .userHtml-placeholder, .userHtml img, .popover--subNavMenu .popover-content {
           border: 1px solid ${darkBorderColor} !important;  /* need full border definition for .bg--off */
+        }
+        .userProfile-header-inner .bg--color-greyPanel {
+          border: 1px solid ${lightBorderColor} !important;
         }
         .bg--color-white, .carousel-list--air, .tabbedInterface-tab--selected, .bg--main, .tabbedInterface-tab--horizontal, .tabbedInterface-tab--selected, .comment--selected, .comments-item--in-moderation, .comments-item-inner--active, .comments-item-inner--edit, /*.thread.cept-sale-event-thread.thread--deal,*/ .vote-btn, .notification-item:not(.notification-item--read), .search div, .search input, .text--overlay, .popover--brandAccent .popover-content, .popover--brandPrimary .popover-content, .popover--default .popover-content, .popover--menu .popover-content, .popover--red .popover-content {
           background-color: ${darkBackgroundColor} !important;
