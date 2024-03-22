e69589ac9bae54ad21c1dfa78bab1a0dd120b8e0
bearbyt3z <bearbyt3z@gmail.com>
2024-03-22 03:10:21 +0100

    Changed the color of the footer text to match the main theme text color

--- PepperTweaker.user.js
+++ PepperTweaker.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         PepperTweaker
 // @namespace    bearbyt3z
-// @version      0.9.91
+// @version      0.9.92
 // @description  Pepper na resorach...
 // @author       bearbyt3z
 // @match        https://www.pepper.pl/*
@@ -387,6 +387,7 @@
       // const orangeColor = '#d1d5db';
 
       css += `
+        footer .text--color-white,
         .text--color-charcoalShade,
         .comments-pagi--header .comments-pagi-pages:not(:disabled),
         .page2-center .mute--text2, .page2-subTitle2.mute--text2, .conversation-content.mute--text2, .linkGrey, .thread-userOptionLink, .cept-nav-subheadline, .user:not(.thread-user), .tabbedInterface-tab, .subNavMenu, .subNavMenu-btn, .tag, .page-label, .page-subTitle, .page2-secTitle, .userProfile-title--sub, .bg--color-inverted .text--color-white, .comments-pagination--header .pagination-next, .comments-pagination--header .pagination-page, .comments-pagination--header .pagination-previous, .conversationList-msgPreview, .thread-title, .mute--text, .text--color-charcoal, .text--color-charcoalTint, .cept-tt, .cept-description-container, /*.cept-tp,*/ .thread-username, .voucher input, .hide--bigCards1, .hide--toBigCards1 {
