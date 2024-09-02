88d237a26e431df925917578f3e8fb59922b71ec
bearbyt3z <bearbyt3z@gmail.com>
2024-09-02 04:25:40 +0200

    Replaced some empty tiles on the search subpage in the grid view

--- PepperTweaker.user.js
+++ PepperTweaker.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         PepperTweaker
 // @namespace    bearbyt3z
-// @version      0.9.127
+// @version      0.9.128
 // @description  Pepper na resorach...
 // @author       bearbyt3z
 // @match        https://www.pepper.pl/*
@@ -3238,7 +3238,8 @@
             .vote-box .cept-show-expired-threads,  /* deal ended text */
             .vote-box span[class^="hide--"],  /* discussion ended text */
             .threadGrid-headerMeta > div > div:not(.vote-box) button,  /* three dots button, covering deal starting date */
-            .js-threadList #exploreMoreRelatedWidget, .js-threadList #exploreMoreTopWidgetPortal {  /* explore more widget */
+            .js-threadList #exploreMoreRelatedWidget, .js-threadList #exploreMoreTopWidgetPortal,  /* explore more widget */
+            #incontent1FuseZonePortal, #incontent2FuseZonePortal, #incontent3FuseZonePortal {  /* empty tiles on the search page */
               display: none;
             }
             .cept-meta-ribbon .icon--refresh {
