f5df0be6160ddc7f89fc444650cfd0c4fa6fd8a2
bearbyt3z <bearbyt3z@gmail.com>
2024-10-25 01:25:36 +0200

    The correction of some comments

--- PepperTweaker.user.js
+++ PepperTweaker.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         PepperTweaker
 // @namespace    bearbyt3z
-// @version      0.9.157
+// @version      0.9.158
 // @description  Pepper na resorach...
 // @author       bearbyt3z
 // @match        https://www.pepper.pl/*
@@ -3264,10 +3264,10 @@
             .vote-box span[class^="hide--"],  /* discussion ended text */
             .threadGrid-headerMeta > div > div:not(.vote-box) button,  /* three dots button, covering deal starting date */
             #exploreMoreRelatedWidget, #exploreMoreTopWidgetPortal,  /* explore more widget */
-            #incontentFuseZonePortal, #incontent1FuseZonePortal, #incontent2FuseZonePortal, #incontent3FuseZonePortal,  /* empty tiles on the search page */
+            #incontentFuseZonePortal, #incontent1FuseZonePortal, #incontent2FuseZonePortal, #incontent3FuseZonePortal,  /* empty tiles on a search page */
             #groupHottestWidgetPortal,  /* hottests deals widget on the category subpage */
             #rlpBannerPortal,  /* link to a voucher subpage on a merchant search page */
-            .js-threadList > div:not([class]):not([id]) { /* empty tiles on the category subpage */
+            .js-threadList > div:not([class]):not([id]) { /* empty tiles on category subpages */
               display: none;
             }
             .cept-meta-ribbon .icon--refresh {
